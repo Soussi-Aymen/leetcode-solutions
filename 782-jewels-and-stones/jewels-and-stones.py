4a3b864b0f89c68099ac66fa160df7f54bb91852
Aymen-Soussi <aymensoussi.02@gmail.com>
2025-07-11 00:35:09 +0200

    Time: 0 ms (100.00%) | Memory: 17.9 MB (10.48%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -1,9 +1,9 @@
 from collections import Counter
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        map_stones = Counter(stones)
+        stones = Counter(stones)
         result = 0
         for jewel in jewels:
-            result += map_stones[jewel]
+            result += stones[jewel]
         
         return result
