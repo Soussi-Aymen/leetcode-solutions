9cacddd861e936e24e950af07377f4d862872e89
Aymen-Soussi <aymensoussi.02@gmail.com>
2025-09-13 00:00:36 +0200

    Time: 23 ms (80.61%) | Memory: 39.6 MB (5.84%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        freq = Counter(nums)
+
+        for element, count in freq.items():
+            if count > 1: 
+                return element
